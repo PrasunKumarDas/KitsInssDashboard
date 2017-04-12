d1c1afb8d3ccb8131df971388f8e5ff9c58ccd2b
KITSPrasunKumarDas <prasun.das@kingfisher.com>
2017-04-12 19:05:10 +0530

    commit

--- www/controllers/dashboard.controller.js
+++ www/controllers/dashboard.controller.js
@@ -2,26 +2,48 @@
     'use strict';
     angular
         .module('ISS-UI')
-        .controller('DashboardController', ['DashboardService','$interval',
+        .controller('DashboardController', ['DashboardService', '$interval',
             function(DashboardService, $interval) {
                 var vm = this;
                 init();
+
                 function init() {
-					 var theInterval = $interval(function(){
-					     loadNotifications();
-					   }.bind(this), 1000);    
-				    loadNotifications();
-                      vm.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-                        vm.data = [300, 500, 100];
-                	
-             	}
-             	function loadNotifications(){
-             		DashboardService.getDashboardData()
-		            .then(function(response){
-		                vm.tillInformation = response.tillHeartBeatReport;
-		            })
-             	}
-               
+                    var theInterval = $interval(function() {
+                        loadNotifications();
+                    }.bind(this), 10000000);
+                    loadNotifications();
+                    loadChart();
+                }
+
+                function loadNotifications() {
+                    DashboardService.getDashboardData()
+                        .then(function(response) {
+                            vm.tillInformation = response.tillHeartBeatReport;
+                        })
+                }
+
+                function loadChart(chartData) {
+                    vm.chartConfig = {
+                        title: {
+                            text: 'Weekly Heartbeat'
+                        },
+                        series: [{
+                            type: 'pie',
+                            data: [{
+                                name: 'Dead',
+                                y: 10
+                            }, {
+                                name: 'Alive',
+                                y: 80,
+                                selected: true
+                            }, {
+                                name: 'P',
+                                y: 10
+                            }]
+                        }]
+                    };
+                }
+
             }
         ]);
 })();
---
+++ www/directives/chartDirective.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('ISS-UI')
+  .directive('chart', [function() {
+    return {
+        restrict: 'E',
+        replace: true,
+        template: '<div></div>',
+        scope: {
+            config: '='
+        },
+        link: function (scope, element, attrs) {
+            var chart;
+            var process = function () {
+                var defaultOptions = {
+                    chart: { renderTo: element[0] },
+                };
+                var config = angular.extend(defaultOptions, scope.config);
+                chart = new Highcharts.Chart(config);
+            };
+            process();
+            scope.$watch("config.series", function (loading) {
+                process();
+            });
+            scope.$watch("config.loading", function (loading) {
+                if (!chart) {
+                    return;
+                }
+                if (loading) {
+                    chart.showLoading();
+                } else {
+                    chart.hideLoading();
+                }
+            });
+        }
+    };
+  }]);  
